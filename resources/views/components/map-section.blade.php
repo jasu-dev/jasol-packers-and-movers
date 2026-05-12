12bd221eecdf5e380caa08dba37e40cc4ee35c76
jasu-dev <gmjthemes@gmail.com>
2026-05-12 14:49:35 +0530

    chore: update contact address and embed new Google Map location

--- resources/views/components/map-section.blade.php
+++ resources/views/components/map-section.blade.php
@@ -1,7 +1,7 @@
 <section class="py-20 px-4 overflow-hidden">
     <div class="container mx-auto rounded-2xl overflow-hidden border border-slate-100 shadow-lg h-120 w-full">
         <iframe
-            src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3781.428607643258!2d73.73334799999999!3d18.599781999999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTjCsDM1JzU5LjIiTiA3M8KwNDQnMDAuMSJF!5e0!3m2!1sen!2sin!4v1771919683885!5m2!1sen!2sin"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3781.4287172138324!2d73.73072417546658!3d18.599777066749485!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2bbe21d8277ad%3A0xc73679f5b4ff881a!2sJasol%20Packers%20and%20Movers!5e0!3m2!1sen!2sin!4v1778577504963!5m2!1sen!2sin"
             width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
     </div>
 </section>
